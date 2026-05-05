c6eeb538e89ca1a335d958222d365fefe3391ddf
marcosmelog-netizen <marcos.melog@gmail.com>
2026-05-04 21:14:28 -0300

    Add DeCertOracle contract to fetch ETH/USD price

--- DeCertOracle.sol
+++ DeCertOracle.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract DeCertOracle {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() {
+        // Feed ETH/USD na Sepolia
+        priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    }
+
+    function getLatestPrice() public view returns (int) {
+        ( , int price, , , ) = priceFeed.latestRoundData();
+        return price; 
+    }
+}
