56e7fbf2812df328c76dcf211a7f916aa5bb3650
marcosmelog-netizen <marcos.melog@gmail.com>
2026-05-04 21:19:59 -0300

    Create DeCertToken contract
    Added DeCertToken contract with ERC20 and Ownable functionality.

--- DeCertToken.sol
+++ DeCertToken.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DeCertToken is ERC20, Ownable {
+    constructor() ERC20("DeCert Token", "DCRT") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
